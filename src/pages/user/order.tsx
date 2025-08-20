49df8130da6849302f00b6b69682be9a77d7a95a
afan <bili_afan@163.com>
2025-08-20 10:47:18 +0800

    250820 路由跳转方式2：navigate跳转和显示隐式传参

--- src/pages/user/order.tsx
+++ src/pages/user/order.tsx
@@ -1,7 +1,36 @@
+import { useNavigate, Navigate } from "umi";
+import { Button } from "antd";
+
+
 const OrderPage = () => {
+  // 使用useNavigate获取navigate函数
+  const navigate = useNavigate();
+  const fn = () => {
+    // console.log("fn...");
+
+    // 方式1 直接跳转到个人信息页面
+    // navigate('/user/profile'); 
+
+
+    // 方式2 使用对象形式跳转，并通过问号传参
+    // navigate({
+    //   pathname: '/user/profile',
+    //   search: '?name=afan&age=18',
+    // })
+
+    // 方式3 使用Navigate组件跳转，并通过隐式传参
+    // ！！！隐式传参的参数，在地址上看不到，且刷新不会丢失！！！
+    navigate('/user/profile',{
+      state: {name: 'afan', age: 18}  // 通过state传参，刷新就没了
+    })
+  }
   return (
     <div>
       <p>我的订单</p>
+      <Button type="primary" onClick={() => {
+        fn()
+      }}> 查看个人信息
+      </Button>
     </div>
   );
 };
--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,4 +1,35 @@
-const ProfilePage = () => {
+import { withRouter, createSearchParams, useSearchParams, useLocation} from "umi";
+
+// props里面的search参数有参数，但是是字符串的问号拼接
+// const ProfilePage = (props) => {
+
+const ProfilePage = ({location}) => {
+  console.log(location);
+
+  // 方式1：使用React V6的URLSearchParams来解析问号参数
+  // const usp = new URLSearchParams(location.search)
+  // console.log(usp.get('name')); // afan
+  // console.log(usp.get('age')); // 18  
+
+  // // 方式2：使用Umi的createSearchParams来解析问号参数
+  // const usp2 = createSearchParams(location.search)
+  // console.log(usp2.get('name')); // afan
+  // console.log(usp2.get('age')); // 18 
+
+  // // 方式3：使用React V6的useSearchParams来解析问号参数
+  // const [usp] = useSearchParams(); // 返回一个数组，第一个是URLSearchParams对象，第二个是设置参数的函数
+  // console.log(usp.get('name')); // afan
+  // console.log(usp.get('age')); // 18  
+
+  // 方式4：隐式传参 使用location.state来获取隐式传参
+  // const usp = new URLSearchParams(location.state)
+  // console.log(usp.get('name')); // afan
+  // console.log(usp.get('age')); // 18  
+
+  // 方式5：使用Umi的useLocation来获取location对象
+  const loc = useLocation();
+  console.log(loc.state); // {name: 'afan', age: 18}  
+
   return (
     <div>
       <p>我的信息</p>
@@ -6,4 +37,4 @@ const ProfilePage = () => {
   );
 };
 
-export default ProfilePage;
+export default withRouter(ProfilePage);
